31632f2c7150a1394cb68d41fe5b01f7022d9eba
Samozvanec1 <usackikhs@yandex.ru>
2024-05-17 00:05:31 +0300

    DZ finisch

--- DZ.py
+++ DZ.py
@@ -1,20 +1,14 @@
-# Шаг 3: Модифицируйте класс Fighter
-#  • Добавьте в класс Fighter поле, которое будет хранить объект класса Weapon.
-#  • Добавьте метод changeWeapon(), который позволяет изменить оружие бойца.
-class Fighter():
-    def __init__(self, oruzie):
-        self.oruzie = oruzie
 
-    def changeOruzie(self):
-        change 
 
-class Monster():
-    pass
+
 
 
 from abc import ABC, abstractmethod
 
 class Weapon(ABC):
+    def __init__(self, name):
+        self.name = name
+
     @abstractmethod
     def attack(self):
         pass
@@ -25,12 +19,47 @@ class Sword(Weapon):
 
 class  Bow(Weapon):
     def attack(self):
-        return "Выстрел грянул, стрела кружит"
+        return ", выстрел грянул, стрела кружит"
+
+
+class Fighter():
+    def __init__(self, oruzie):
+        self.oruzie = oruzie
+
+    def changeOruzie(self, smena_or):
+        self.oruzie = smena_or
+
+    def attack(self):
+        return self.oruzie.attack()
+
+class Monster():
+    def __init__(self):
+        pass
+
+
+def battle(voin, monster):
+    print("Воин выбирает", voin.oruzie.name)
+    print("Воин наносит", voin.attack())
+    print("Монстр побежден!")
+
+def schet(chisla):
+    for i in range(chisla):
+        print(i+1)
 
 
 
+sword = Sword("Дюрандаль")
+bow  = Bow("Гандива")
+fighter = Fighter(sword)
+monster = Monster()
 
+battle(fighter, monster)
 
+print(f"\n     Считаем до пяти, и монстр воскреснет с уменеем биться против меча ")
 
+schet(5)
+print("\n")
 
+fighter.changeOruzie(bow)
+battle(fighter, monster)
 
